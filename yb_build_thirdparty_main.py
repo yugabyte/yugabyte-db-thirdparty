cf4179734f9796ef649dd10c5b478218b86253ae
Mikhail Bautin <mbautin@users.noreply.github.com>
2020-10-16 00:15:02 -0500

    Don't add special flags to libcxx for ASAN/TSAN [skip ci]

--- yb_build_thirdparty_main.py
+++ yb_build_thirdparty_main.py
@@ -1003,14 +1003,17 @@ class Builder(BuilderInterface):
         # Special setup for Clang on Linux.
         # -----------------------------------------------------------------------------------------
 
-        if self.build_type == BUILD_TYPE_ASAN:
-            self.compiler_flags += [
-                '-fsanitize=address',
-                '-fsanitize=undefined',
-                '-DADDRESS_SANITIZER'
-            ]
-        if self.build_type == BUILD_TYPE_TSAN:
-            self.compiler_flags += ['-fsanitize=thread', '-DTHREAD_SANITIZER']
+        is_libcxx = self.dep.startswith('libcxx')
+
+        if not is_libcxx:
+            if self.build_type == BUILD_TYPE_ASAN:
+                self.compiler_flags += [
+                    '-fsanitize=address',
+                    '-fsanitize=undefined',
+                    '-DADDRESS_SANITIZER'
+                ]
+            if self.build_type == BUILD_TYPE_TSAN:
+                self.compiler_flags += ['-fsanitize=thread', '-DTHREAD_SANITIZER']
 
         if self.args.single_compiler_type == 'clang':
             self.init_clang10_or_later_flags(dep)
