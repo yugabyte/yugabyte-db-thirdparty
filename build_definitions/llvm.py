14b4a12341102879a85f9355df626eeb93d3527a
Mikhail Bautin <mbautin@users.noreply.github.com>
2020-10-25 23:21:42 -0500

    Set LLVM_INCLUDE_TESTS to ON for llvm7 [skip ci] -- needed to build gtest

--- build_definitions/llvm.py
+++ build_definitions/llvm.py
@@ -78,7 +78,7 @@ class LLVMDependency(Dependency):
                 '-DCMAKE_BUILD_TYPE=Release',
                 '-DLLVM_INCLUDE_DOCS=OFF',
                 '-DLLVM_INCLUDE_EXAMPLES=OFF',
-                '-DLLVM_INCLUDE_TESTS=OFF',
+                '-DLLVM_INCLUDE_TESTS=ON',
                 '-DLLVM_INCLUDE_UTILS=OFF',
                 '-DLLVM_TARGETS_TO_BUILD=X86',
                 '-DLLVM_ENABLE_RTTI=ON',
