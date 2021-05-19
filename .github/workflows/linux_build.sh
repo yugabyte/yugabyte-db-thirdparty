13e6a0104886e8e0c891e4936564814039a9ae67
Mikhail Bautin <mbautin@users.noreply.github.com>
2021-05-18 17:24:36 -0700

    Set up the 2.4 branch of the yugabyte-db-thirdparty repo (#64)
    - Branch off of the commit 45c97f45f1ea2f0898b87f434439c8356c119761, which is the thirdparty commit used in the 2.4 branch of yugabyte/yugabyte-db ( https://github.com/yugabyte/yugabyte-db/tree/2.4 ), which is at the commit yugabyte/yugabyte-db@bcf406cf3ee42cb4944e24154de744378cb48ec7 as of this writing.
    - GitHub Actions setup similar to the master branch of the yugabyte-db-thirdparty repo (as of the 6a4d8d4628ad21c19dba9eae29f3115c2337ef06 commit).
    - Downgrade gperftools to 2.7.
    - Fix the Boost source tarball download URL.
    - Remove the unused multi_build.py / MultiBuilder functionality.
    - Add various possible checksums that we might later need to thirdparty_src_checksums.txt.
    - Use the codecheck tool for running mypy on Python code and shellcheck on Bash code.

---
+++ .github/workflows/linux_build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+df -H .
+checkout_dir=$PWD
+echo "Building in directory: $checkout_dir"
+docker run -t \
+  --cap-add=SYS_PTRACE \
+  -e GITHUB_TOKEN \
+  -e YB_BUILD_THIRDPARTY_ARGS \
+  -e YB_THIRDPARTY_ARCHIVE_NAME_SUFFIX \
+  "--mount=type=bind,src=$checkout_dir,dst=/opt/yb-build/thirdparty/checkout" \
+  "$YB_DOCKER_IMAGE" \
+  bash -c "
+    set -euo pipefail
+    cd /opt/yb-build/thirdparty/checkout
+    ./build_and_release.sh
+  "
