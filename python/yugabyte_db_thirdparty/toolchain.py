276035a517fd256ce49157b265c384b4ed6a0a8a
Mikhail Bautin <mbautin@users.noreply.github.com>
2021-10-02 19:50:54 -0700

    Use toolchain builds made on a compatible OS (e.g. RedHat compatible variants)

--- python/yugabyte_db_thirdparty/toolchain.py
+++ python/yugabyte_db_thirdparty/toolchain.py
@@ -20,7 +20,7 @@ from yugabyte_db_thirdparty.util import YB_THIRDPARTY_DIR, write_file
 from typing import Optional
 
 import sys_detection
-from sys_detection import SHORT_OS_NAME_REGEX_STR
+from sys_detection import SHORT_OS_NAME_REGEX_STR, is_compatible_os_and_version
 
 
 LINUXBREW_URL = (
@@ -112,6 +112,12 @@ class Toolchain:
         return url_match.group(1)
 
 
+def is_compatible_os_arch_combination(os_arch1: str, os_arch2: str) -> bool:
+    os1, arch1 = os_arch1.split('-')
+    os2, arch2 = os_arch2.split('-')
+    return is_compatible_os_and_version(os1, os2) and arch1 == arch2
+
+
 def ensure_toolchain_installed(
         download_manager: DownloadManager,
         toolchain_type: str) -> Toolchain:
@@ -123,13 +129,25 @@ def ensure_toolchain_installed(
     os_and_arch_to_url = TOOLCHAIN_TO_OS_AND_ARCH_TO_URL[toolchain_type]
     local_sys_conf = sys_detection.local_sys_conf()
     os_and_arch = local_sys_conf.id_for_packaging()
-    if os_and_arch not in os_and_arch_to_url:
-        raise ValueError(
-                f"Toolchain {toolchain_type} not found for OS/architecture combination "
-                f"{os_and_arch}")
+    if os_and_arch in os_and_arch_to_url:
+        toolchain_url = os_and_arch_to_url[os_and_arch]
+    else:
+        os_and_arch_candidates = []
+        for os_and_arch_candidate in os_and_arch_to_url.keys():
+            if is_compatible_os_arch_combination(os_and_arch_candidate, os_and_arch):
+                os_and_arch_candidates.append(os_and_arch_candidate)
+        err_msg_prefix = (
+            f"Toolchain {toolchain_type} not found for OS/architecture combination {os_and_arch}")
+        if not os_and_arch_candidates:
+            raise ValueError(
+                    f"{err_msg_prefix}, and no compatible OS/architecture combinations found.")
+        if len(os_and_arch_candidates) > 1:
+            raise ValueError(
+                    f"{err_msg_prefix}, and too many compatible OS/architecture combinations "
+                    "found, cannot choose automatically: {os_and_arch_candidates}")
+        effective_os_and_arch = os_and_arch_candidates[0]
+        toolchain_url = os_and_arch_to_url[effective_os_and_arch]
 
-    toolchain_url = os_and_arch_to_url[os_and_arch]
-    compiler_type = None
     if toolchain_type.startswith('llvm'):
         parent_dir = '/opt/yb-build/llvm'
     elif toolchain_type == 'linuxbrew':
