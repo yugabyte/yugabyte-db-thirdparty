bcfe6204eef259505ecaa4a9bebf51a6dc26efaf
Bharat Kumar <bkumar@yugabyte.com>
2022-06-09 20:26:54 +0000

    fix lint errors

--- python/yugabyte_db_thirdparty/yb_build_thirdparty_main.py
+++ python/yugabyte_db_thirdparty/yb_build_thirdparty_main.py
@@ -100,7 +100,7 @@ def main() -> None:
             log("SNYK_TOKEN is not set, not running snyk.")
         else:
             log("Running Snyk Vulnerability Scan.")
-            os_type=os.environ.get('OSTYPE', '')
+            os_type = os.environ.get('OSTYPE', '')
             if os_type.startswith('linux'):
                 os.system("curl https://static.snyk.io/cli/latest/snyk-linux -o snyk")
                 os.system("chmod +x ./snyk")
@@ -110,6 +110,7 @@ def main() -> None:
                 else:
                     homedir = os.environ.get('YB_THIRDPARTY_DIR')
                     os.system(f"./snyk monitor {homedir}/src --unmanaged")
+
 
 if __name__ == "__main__":
     main()
