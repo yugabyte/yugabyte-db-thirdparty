d3b57c4f14ec720488bc3f2b70ace406d96454e7
Mikhail Bautin <552936+mbautin@users.noreply.github.com>
2023-06-28 08:46:58 -0700

    Add AlmaLinux 9 build types for x86_64 (#204)
    Adding AlmaLinux 9 and GCC 12 build types.
    
    CI build types: centos7-x86_64-clang16, almalinux8-x86_64-clang16, almalinux9-x86_64-clang16, almalinux9-x86_64-gcc12, centos7-aarch64-clang16
    
    The above "CI build types:" syntax is a new syntax in the commit message that allows us to only build a subset of build types for a particular commit / pull request. The build jobs are still started for all build types, but they finish very quickly if the build type is filtered out.
    
    Also upgrade LLVM 16 version to 16.0.6-yb-2 to increase lock array sizes to try to work around the TSAN issue https://github.com/yugabyte/yugabyte-db/issues/17511.

---
+++ .github/workflows/github_actions_build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+readonly CI_BUILD_TYPES_KEYWORD="CI build types:"
+GIT_HEAD_COMMIT_MESSAGE=$( git log --format=%B -n 1 --no-merges )
+readonly GIT_HEAD_COMMIT_MESSAGE
+
+echo "Git HEAD commit message: $GIT_HEAD_COMMIT_MESSAGE"
+
+# A quick way to filter build types in the commit message.
+if [[ $GIT_HEAD_COMMIT_MESSAGE != *"$CI_BUILD_TYPES_KEYWORD"* ||
+      $GIT_HEAD_COMMIT_MESSAGE == \
+          *"$CI_BUILD_TYPES_KEYWORD"*"$YB_THIRDPARTY_ARCHIVE_NAME_SUFFIX"* ]]; then
+  echo "Git HEAD commit message does not specify a build type filter, or the filter" \
+       "matches build type: $YB_THIRDPARTY_ARCHIVE_NAME_SUFFIX"
+  if [[ $OSTYPE == darwin* ]]; then
+    ./.github/workflows/macos_build.sh
+  else
+    ./.github/workflows/linux_build.sh
+  fi
+else
+  echo "Build type $YB_THIRDPARTY_ARCHIVE_NAME_SUFFIX filtered out by the" \
+       "'$CI_BUILD_TYPES_KEYWORD' commit message filter."
+fi
