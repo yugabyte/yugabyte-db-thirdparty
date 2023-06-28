096ced3c9adf03d6c6ab824aa123419bacc29dfe
Mikhail Bautin <552936+mbautin@users.noreply.github.com>
2023-06-27 17:42:23 -0700

    Refactor test script and turn the build back on. CI build types: almalinux9-x86_64-clang16, almalinux9-x86_64-gcc12

--- .github/workflows/github_actions_build.sh
+++ .github/workflows/github_actions_build.sh
@@ -2,22 +2,23 @@
 
 set -euo pipefail
 
-GIT_HEAD_COMMIT_MESSAGE=$( git log --format=%B -n 1 --no-merges )
-echo "GIT head commit message: $GIT_HEAD_COMMIT_MESSAGE"
+readonly CI_BUILD_TYPES_KEYWORD="CI build types:"
+readonly GIT_HEAD_COMMIT_MESSAGE=$( git log --format=%B -n 1 --no-merges )
+
+echo "Git HEAD commit message: $GIT_HEAD_COMMIT_MESSAGE"
 
 # A quick way to filter build types in the commit message.
-if [[ $GIT_HEAD_COMMIT_MESSAGE != *"CI build types:"* ||
+if [[ $GIT_HEAD_COMMIT_MESSAGE != *"$CI_BUILD_TYPES_KEYWORD"* ||
       $GIT_HEAD_COMMIT_MESSAGE == \
-          *"CI build types:"*"$YB_THIRDPARTY_ARCHIVE_NAME_SUFFIX"* ]]; then
-  echo "GitHub HEAD commit message does not specify a build type filter, or the filter" \
-        "matches build type: $YB_THIRDPARTY_ARCHIVE_NAME_SUFFIX"
-  if false; then
-    if [[ $OSTYPE == darwin* ]]; then
-      ./.github/workflows/macos_build.sh
-    else
-      ./.github/workflows/linux_build.sh
-    fi
+          *"$CI_BUILD_TYPES_KEYWORD"*"$YB_THIRDPARTY_ARCHIVE_NAME_SUFFIX"* ]]; then
+  echo "Git HEAD commit message does not specify a build type filter, or the filter" \
+       "matches build type: $YB_THIRDPARTY_ARCHIVE_NAME_SUFFIX"
+  if [[ $OSTYPE == darwin* ]]; then
+    ./.github/workflows/macos_build.sh
+  else
+    ./.github/workflows/linux_build.sh
   fi
 else
-  echo "Build type filtered out by commit message: $YB_THIRDPARTY_ARCHIVE_NAME_SUFFIX"
+  echo "Build type $YB_THIRDPARTY_ARCHIVE_NAME_SUFFIX filtered out by the" \
+       "'$CI_BUILD_TYPES_KEYWORD' commit message filter."
 fi
