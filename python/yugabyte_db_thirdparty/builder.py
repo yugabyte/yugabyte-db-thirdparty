b98005d8f4c88d90ec18ddf1efce66470ed0c716
Saurav Tiwary <srv.twry@gmail.com>
2023-07-27 10:58:01 +0530

    [BACKPORT 2.18] Add build definitions for jwt-cpp (#208)
    Backport #205 to the 2.18 branch as well.
    
    JWT-CPP is a header-only library providing Json Web Token functionalities.
    
    We also updated the protobuf build definition to download gmock and gtest as part of protobuf download to skip the it during build which leads to build failures on mac11 build.

--- python/yugabyte_db_thirdparty/builder.py
+++ python/yugabyte_db_thirdparty/builder.py
@@ -315,6 +315,7 @@ class Builder(BuilderInterface):
             'openldap',
             'redis_cli',
             'wyhash',
+            'jwt_cpp',
         ])
         for dep in self.dependencies:
             if dep.build_group != BUILD_GROUP_COMMON:
