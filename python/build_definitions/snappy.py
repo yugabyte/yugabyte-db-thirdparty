cc4d2af0ec43d8ce5e148df6440ae4a22d50f8c2
Anubhav Srivastava <SrivastavaAnubhav@users.noreply.github.com>
2023-10-19 00:40:25 -0700

    Upgrade Snappy to 1.1.9-yb-3 to fix GCC 11 (#238)
    Backport the snappy upgrade from
    https://github.com/yugabyte/yugabyte-db-thirdparty/commit/5ea869b800a5173f116921ea03a33604143748e2
    to fix GCC 11 builds running into this longjmp error:
    https://github.com/yugabyte/yugabyte-db/issues/12429.

--- python/build_definitions/snappy.py
+++ python/build_definitions/snappy.py
@@ -21,45 +21,16 @@ class SnappyDependency(Dependency):
     def __init__(self) -> None:
         super(SnappyDependency, self).__init__(
             name='snappy',
-            version='1.1.3',
-            url_pattern='https://github.com/google/snappy/archive/{0}.tar.gz',
+            version='1.1.9-yb-3',
+            url_pattern='https://github.com/yugabyte/snappy/archive/refs/tags/v{0}.tar.gz',
             build_group=BUILD_GROUP_INSTRUMENTED)
-        self.copy_sources = True
-        self.patch_version = 1
-        self.patches = ['snappy-define-guard-macro.patch']
-        self.post_patch = ['autoreconf', '-fvi']
-
-    def _disable_lzo2_library_in_test(self) -> None:
-        '''
-        Makes the snappy unit test not use the liblzo2 library. Sometimes the configure script will
-        pick up the library from a system directory when it should not.
-        '''
-        log("Removing HAVE_LIBLZO2 from config.h")
-        lines: List[str] = []
-        removed = False
-        with open('config.h') as input_file:
-            for line in input_file:
-                if line.strip() == '#define HAVE_LIBLZO2 1':
-                    removed = True
-                else:
-                    lines.append(line)
-        if not removed:
-            log("Warning: did not remove HAVE_LIBLZO2 from config.h")
-        with open('config.h', 'w') as output_file:
-            output_file.write('\n'.join(lines) + '\n')
 
     def build(self, builder: BuilderInterface) -> None:
-        log_prefix = builder.log_prefix(self)
-        builder.build_with_configure(
-            log_prefix=log_prefix,
-            extra_args=['--with-pic'],
-            post_configure_action=self._disable_lzo2_library_in_test,
+        builder.build_with_cmake(
+            dep=self,
+            extra_args=[
+                '-DSNAPPY_BUILD_TESTS=OFF',
+                '-DSNAPPY_BUILD_BENCHMARKS=OFF',
+            ],
+            shared_and_static=True
         )
-        # Copy over all the headers into a generic include/ directory.
-        mkdir_if_missing('include')
-        subprocess.check_call('ls | egrep "snappy.*.h" | xargs -I{} rsync -av "{}" "include/"',
-                              shell=True)
-
-        # Copy over all the libraries into a generic lib/ directory.
-        mkdir_if_missing('lib')
-        subprocess.check_call('ls ".libs/" | xargs -I{} rsync -av ".libs/{}" "lib/"', shell=True)
