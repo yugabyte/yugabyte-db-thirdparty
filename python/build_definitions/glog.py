d0280d74d4f95ee7610c5fe3c8bf2de59a778e2c
Mikhail Bautin <552936+mbautin@users.noreply.github.com>
2023-10-25 16:47:58 -0700

    Remove v when downloading a commit

--- python/build_definitions/glog.py
+++ python/build_definitions/glog.py
@@ -24,7 +24,7 @@ class GLogDependency(Dependency):
         super(GLogDependency, self).__init__(
             name='glog',
             version='ad75104e5606c42bc938b0d0380145a8b19a0d9f',
-            url_pattern='https://github.com/mbautin/glog/archive/v{0}.tar.gz',
+            url_pattern='https://github.com/mbautin/glog/archive/{0}.tar.gz',
             build_group=BuildGroup.POTENTIALLY_INSTRUMENTED)
         self.patch_version = 1
         self.patch_strip = 0
