cd738ce9a676ebd8eb3ec0ccfe2a63e1b37dca13
Hari Krishna Sunder <hari90@users.noreply.github.com>
2025-03-20 16:37:21 -0700

    Add bson

--- python/yugabyte_db_thirdparty/dependency_selection.py
+++ python/yugabyte_db_thirdparty/dependency_selection.py
@@ -85,6 +85,7 @@ def get_final_dependency_module_names(compiler_choice: CompilerChoice) -> List[s
         'hdrhistogram',
         'otel_proto',
         'otel',
+        'bson',
     ])
 
     if is_linux() and is_building_for_x86_64() and (
