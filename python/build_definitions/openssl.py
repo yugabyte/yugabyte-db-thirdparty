a8302dcb6fd1801a9135d2f32ae3cf65970ebbf3
Eric Sheng <esheng@yugabyte.com>
2025-06-05 15:36:41 -0700

    [2024.1] Bump OpenSSL to 3.0.16

--- python/build_definitions/openssl.py
+++ python/build_definitions/openssl.py
@@ -34,7 +34,7 @@ class OpenSSLDependency(Dependency):
     def __init__(self) -> None:
         super(OpenSSLDependency, self).__init__(
             name='openssl',
-            version='3.0.15',
+            version='3.0.16',
             url_pattern='https://www.openssl.org/source/openssl-{0}.tar.gz',
             build_group=BuildGroup.COMMON)
         self.copy_sources = True
