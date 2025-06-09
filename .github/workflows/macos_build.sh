5fa8c806a193199e83343daaff5a44cbd58dc8ac
Eric Sheng <esheng@yugabyte.com>
2025-06-09 16:16:23 -0700

    [2.20] Bump OpenSSL to 3.0.16 (#318)
    Context: https://github.com/yugabyte/yugabyte-db/issues/27491
    
    Original commit: 96bf8ffaeb8c5005076d61b560eb74b379fd2d41
    
    Also pulled in changes to:
    - use top-level VMs to Ubuntu 24.04 (20.04 now unsupported)
    (27b82bf1a139d6bf6f1f8bb3aa2df2f16b4d99de)
    - use macos-13 (macos-12 now unsupported)
    (d4cf290eedeb2daeaa8617f9c129bcd1044b4448)
    - use Python 3.9 (3.13 is used on macos-13 which doesn't work)
    (fa66f9704082404c79deab0e1ee82af4f4e63300)
    - bumped actions/upload-artifacts to v4 (v3 now unsupported)
    (58159468930f7a954e1a060e9d4b8a30e490b500)
    - update boost URL to match the one used on master (old one no longer
    works) (d6c99280abe80735f93d70a6110a48f7055970bd)
    - manually install CMake 3 on OS X (brew will install CMake 4+ which
    doesn't work) (4a788e7836f792f085661c28a8d57191a4c77274)

--- .github/workflows/macos_build.sh
+++ .github/workflows/macos_build.sh
@@ -6,4 +6,6 @@ brew install autoconf automake pkg-config shellcheck hub
 dirs=( /opt/yb-build/{thirdparty,brew,tmp} )
 sudo mkdir -p "${dirs[@]}"
 sudo chmod 777 "${dirs[@]}"
+
+export PYTHON="python3.9"
 ./build_and_release.sh
