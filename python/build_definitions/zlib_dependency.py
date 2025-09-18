5f9bb41a471c6585a86eaf555a7a728756127bcf
Jeff Harvey-Smith <jharveysmith@yugabyte.com>
2025-09-18 10:48:44 -0700

    Bump zlib version to 1.3.1

--- python/build_definitions/zlib_dependency.py
+++ python/build_definitions/zlib_dependency.py
@@ -22,7 +22,7 @@ class ZLibDependency(Dependency):
     def __init__(self) -> None:
         super(ZLibDependency, self).__init__(
             name='zlib',
-            version='1.2.13-yb-1',
+            version='1.3.1-yb-1',
             url_pattern='https://github.com/yugabyte/zlib/archive/refs/tags/v{0}.tar.gz',
             build_group=BuildGroup.COMMON)
         self.copy_sources = True
